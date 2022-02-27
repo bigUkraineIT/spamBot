a3d6b4fa1e5215d02850a5d60cf5a0b1bd76f6a1
bigUkraineIT <bigUkraineIT@pm.me>
2022-02-27 16:13:27 +0200

    Update

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ print("Api_id: ")
 api_id = input()
 print("Hash_id: ")
 api_hash = input()
+print("Username to spam: ")
+username = input()
 print("Text to spam: ")
 spamText = input()
 
 with TelegramClient('name', api_id, api_hash) as client:
    while (1!=0):
       for i in range(12):
-         client.send_message('vsu_info_bot', spamText)
+         client.send_message(username, spamText)
       print("12 messages have been sended! Sleep for 6 sec")
       time.sleep(6)
 
